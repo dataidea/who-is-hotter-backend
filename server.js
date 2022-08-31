c32b5462b96dbd06941aea4d4e2a7d1f1e708737
jumashafara <jumashafara0@gmail.com>
2022-08-31 14:31:52 +0300

    added required packages

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
 const cors = require("cors");
-const multer = require("multer");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const Subject = require("./models/subject");
